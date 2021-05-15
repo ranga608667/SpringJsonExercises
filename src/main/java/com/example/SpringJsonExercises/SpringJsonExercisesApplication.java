418b9d630a11de598e03593d889fe8369ab8d8de
ranga608667 <rangarajan.murugan@cognizant.com>
2021-05-15 13:26:38 -0500

    initial commit

---
+++ src/main/java/com/example/SpringJsonExercises/SpringJsonExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringJsonExercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJsonExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJsonExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringJsonExercises/SpringJsonExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringJsonExercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJsonExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
